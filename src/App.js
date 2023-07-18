0699701a841ef777cfee745cac71141dd14ea713
Tullio Rauber <raubert@outlook.com>
2023-07-18 16:00:32 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <div className="app">
-      <ToastContainer style={{marginTop: 120}} autoClose={3000}/>
+      <ToastContainer style={{marginTop: 121}} autoClose={3000}/>
       <RoutesApp/>
     </div>
   );
